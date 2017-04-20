cff57af460e524fc4e276b64666a3e498d2bf9b4
Daniel Ruatta <druatta@ucsc.edu>
2017-04-20 12:54:10 -0700

    Adding subplots

--- SingleImageContrastCalculation.m
+++ SingleImageContrastCalculation.m
@@ -1,22 +1,24 @@
 close all;
 CameraPhoto = ReadImageFromWorkspace('12V_400nsStrobePulse_Image.png');
 
-figure('Name', 'Camera Photo')
+subplot(221);
 DisplayImage(CameraPhoto);
-figure('Name', 'Camera Photo Gray Intensity');
-CreateHistogram(CameraPhoto);
+title('Camera Photo');
+
+subplot(222);
+imhist(CameraPhoto);
+title('Camera Photo Intensity');
 
 % We use histogram equalization: 
 % Histogram equalization paper: https://www.math.uci.edu/icamp/courses/math77c/demos/hist_eq.pdf
 % Histogram equalization video: https://www.youtube.com/watch?v=PD5d7EKYLcA
-OneHistogramEqualization = histeq(GrayscaleImage); 
-figure('Name', 'One histogram equalizion: Image quality');
-DisplayImage(OneHistogramEqualization);
-figure('Name', 'One histogram equalizion: Gray Intensity');
-CreateHistogram(OneHistogramEqualization);
 
-TwoHistogramEqualizations = histeq(OneHistogramEqualization);
-figure ('Name', 'Two histogram equalizations: Image quality');
-DisplayImage(TwoHistogramEqualizations);
-figure('Name', 'Two histogram equalizations: Gray Intensity')
-CreateHistogram(TwoHistogramEqualizations);
+OneHistogramEqualization = histeq(CameraPhoto); 
+
+subplot(223);
+DisplayImage(OneHistogramEqualization);
+title('One histogram equalizion: Image quality');
+
+subplot(224);
+imhist(OneHistogramEqualization);
+title('One histogram equalizion: Gray Intensity');
