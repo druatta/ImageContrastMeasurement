205aaa7a7ad0797042f84df50f67414983f9b7d8
druatta <druatta@ucsc.edu>
2017-04-24 21:12:08 -0700

    I want to label each pixel well
    Try using this to get started:
    https://www.mathworks.com/help/images/ref/bwselect.html

--- TestingImageEnhancementOnSubstratePhoto.m
+++ TestingImageEnhancementOnSubstratePhoto.m
@@ -7,27 +7,22 @@ figure
 I = RunHistogramEqualization(CameraPhoto);
 DisplayImage(I)
 
+I3 = imadjust(I);
+imshow(I3);
 
-BW1 = edge(I,'canny');
-figure('Name', 'Canny filter');
-imshow(BW1)
-
-
-% % Testing connected components
-% cc = bwconncomp(bw, 8)
-% cc.NumObjects
-% labeled = labelmatrix(cc);
-% RGB_label = label2rgb(labeled, @spring, 'c', 'shuffle');
-% imshow(RGB_label)
-% 
-% graindata = regionprops(cc, 'basic')
+bw = imbinarize(I3);
+bw = bwareaopen(bw, 50);
+imshow(bw)
 
 
-% I3 = imadjust(I);
-% imshow(I3);
-% 
-% bw = imbinarize(I3);
-% bw = bwareaopen(bw, 50);
-% imshow(bw)
+% Testing connected components
+cc = bwconncomp(bw, 8)
+cc.NumObjects
+labeled = labelmatrix(cc);
+I3 = imadjust(labeled);
+DisplayImage(I3);
+RGB_label = label2rgb(labeled, @spring, 'c', 'shuffle');
+%imshow(RGB_label)
 
+graindata = regionprops(cc, 'basic')
 
