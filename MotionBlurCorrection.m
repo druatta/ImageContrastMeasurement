e74397269cc752da27b1b564f91137b091fc5a86
druatta <druatta@ucsc.edu>
2017-04-25 20:20:29 -0700

    Need to run these tests

---
+++ MotionBlurCorrection.m
@@ -0,0 +1,16 @@
+CameraPhoto = ReadImageFromWorkspace('12V_400nsStrobePulse_Image.png');
+figure('Name', 'Camera Photo: Image Quality');
+DisplayImage(CameraPhoto);
+
+figure('Name', 'Camera Photo: Gray Intensity Histogram');
+CreateHistogram(CameraPhoto);
+
+% We use histogram equalization: 
+% Histogram equalization paper: https://www.math.uci.edu/icamp/courses/math77c/demos/hist_eq.pdf
+% Histogram equalization video: https://www.youtube.com/watch?v=PD5d7EKYLcA
+figure('Name', 'One Histogram Equalizion: Image Quality')
+PhotoAfterHistogramEqualization = RunHistogramEqualization(CameraPhoto); 
+DisplayImage(PhotoAfterHistogramEqualization);
+
+figure('Name', 'One Histogram Equalizion: Gray Intensity Histogram');
+CreateHistogram(PhotoAfterHistogramEqualization);
