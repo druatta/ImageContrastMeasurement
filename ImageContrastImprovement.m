2045c8569f11d964fa30800890ab4327aa671487
druatta <druatta@E2516PC02.ad.crm.ucsc.edu>
2017-05-07 21:49:52 -0700

    Testing git commit all

--- ImageContrastImprovement.m
+++ ImageContrastImprovement.m
@@ -1,16 +1,20 @@
 close all;
 
-CameraPhoto = ReadImageFromWorkspace('[1,30] (1).bmp');
-figure('Name', 'Camera Photo: Image Quality');
+CameraPhoto = ReadImageFromWorkspace('12V_400ns.png');
+figure('Name', 'Original Photo');
 DisplayImage(CameraPhoto);
 
 % We use histogram equalization: 
 % Histogram equalization paper: https://www.math.uci.edu/icamp/courses/math77c/demos/hist_eq.pdf
 % Histogram equalization video: https://www.youtube.com/watch?v=PD5d7EKYLcA
-figure('Name', 'One Histogram Equalizion: Image Quality')
+figure('Name', 'Histogram Equalization')
 PhotoAfterHistogramEqualization = HistogramEqualization(CameraPhoto); 
 DisplayImage(PhotoAfterHistogramEqualization);
 
-figure('Name', 'Photo after Histogram Equalization and Contrast Stretching')
+figure('Name', 'Histogram Equalization then Contrast Stretching')
 PhotoAfterContrastStretching = ContrastStretching(PhotoAfterHistogramEqualization);
 DisplayImage(PhotoAfterContrastStretching);
+
+figure('Name', 'Histogram Equalization then Contrast Stretching then 3x3 Averaging Filter')
+PhotoAfterAveragingFilter = filter2(fspecial('average',2),PhotoAfterContrastStretching)/255;
+DisplayImage(PhotoAfterAveragingFilter);
